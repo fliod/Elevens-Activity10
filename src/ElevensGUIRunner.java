e2ea2c5fe60356737f9422e81f2299268530034a
fliod <kgraceaz@gmail.com>
2015-03-23 18:29:15 -0700

    Update ElevensGUIRunner.java

--- src/ElevensGUIRunner.java
+++ src/ElevensGUIRunner.java
@@ -1,14 +1,16 @@
 /**
- * This is a class that plays the GUI version of the Thirteens game.
- * See accompanying documents for a description of how Thirteens is played.
+ * This is a class that plays the GUI version of the Elevens game.
+ * See accompanying documents for a description of how Elevens is played.
  */
-public class ThirteensGUIRunner {
+public class ThirteensGUIRunner 
+{
 
  /**
-  * Plays the GUI version of Thirteens.
+  * Plays the GUI version of Elevens.
   * @param args is not used.
   */
- public static void main(String[] args) {
+ public static void main(String[] args)
+ {
   Board board = new ThirteensBoard();
   CardGameGUI gui = new CardGameGUI(board);
   gui.displayGame();
